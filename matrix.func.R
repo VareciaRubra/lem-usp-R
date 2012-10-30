47ce757e337ffe0ad6aff7861ca0e4aa995929fa
diogro <diogro@gmail.com>
2012-10-30 12:50:55 -0200

    Changed mod.main to TestModularity
    plus style changes

--- matrix.func.R
+++ matrix.func.R
@@ -1,6 +1,6 @@
 RemoveSize <- function (cov.matrix){
   # Removes first principal component effect in cov.matrix.
-  # 
+  #
   # Args:
   #   cov.matrix: A simetric covariance matrix
   # Return:
@@ -13,32 +13,36 @@ RemoveSize <- function (cov.matrix){
   return (cov.matrix.size.removed)
 }
 
-mod.main <- function (cor, modhip, nit = 1000)
-  {
-    no.hip <- dim (modhip) [2]
-    traits <- dim (modhip) [1]
-    m.hip.array <- array (0, c(traits, traits, no.hip + 1))
-    for (N in 1:no.hip)
-      {
-        for (L in 1:traits)
-          {
-            for (M in 1:traits)
-              {
-                m.hip.array[L,M,N] <- ifelse (modhip[L,N] & modhip[M,N], 1, 0)
-              }
-          }
+TestModularity <- function (cor.matrix, modularity.hipot) {
+  # Tests modularity hipotesis using cor.matrix matrix and trait groupings
+  #
+  # Args:
+  #   cor.matrix: Correlation matrix
+  #   modularity.hipot: Matrix of hipotesis.
+  #                     Each line represents a trait and each column a module.
+  #                     if modularity.hipot[i,j] == 1, trait i is in module j.
+  # Return:
+  #   list with mantel correlation of cor.matrix with binary hipotesis matrices
+  no.hip <- dim (modularity.hipot) [2]
+  traits <- dim (modularity.hipot) [1]
+  m.hip.array <- array (0, c(traits, traits, no.hip + 1))
+  for (N in 1:no.hip){
+    for (L in 1:traits){
+      for (M in 1:traits){
+        m.hip.array[L,M,N] <- ifelse (modularity.hipot[L,N] & modularity.hipot[M,N], 1, 0)
       }
-    m.hip.array[,,no.hip+1] <- as.integer (as.logical (apply (m.hip.array, c(1,2), sum)))
-    no.hip <- no.hip + 1
-    output <- array (0, c(5, no.hip))
-    for (N in 1:no.hip)
-      {
-        tmp <- mantel (cor, m.hip.array[,,N], mod = TRUE)
-        output[,N] <- tmp
-      }
-    dimnames(output) <- list (names (tmp), c (colnames (modhip),"Full Integration"))
-    return (output)
+    }
   }
+  m.hip.array[,,no.hip+1] <- as.integer (as.logical (apply (m.hip.array, c(1,2), sum)))
+  no.hip <- no.hip + 1
+  output <- array (0, c(5, no.hip))
+  for (N in 1:no.hip){
+    tmp <- mantel (cor.matrix, m.hip.array[,,N], mod = TRUE)
+    output[,N] <- tmp
+  }
+  dimnames(output) <- list (names (tmp), c (colnames (modularity.hipot),"Full Integration"))
+  return (output)
+}
 
 MultiRsMantel <- function (matrix.list, matrix.comp.func = RandomSkewers,
                            repeat.vector = NULL, iterations = 1000)
