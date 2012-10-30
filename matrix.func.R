e6831741b1de50173f5efb37f698fbea89553793
diogro <diogro@gmail.com>
2012-10-30 13:46:18 -0200

    Style changes to MantelCor

--- matrix.func.R
+++ matrix.func.R
@@ -252,6 +252,7 @@ monte.carlo.rep <- function (matrix, ind, nit = 100)
   }
 
 Norm = function(x){return(sqrt(sum(x*x)))}
+
 Normalize = function(x){return(x/Norm(x))}
 
 RandomSkewers <- function (cov.matrix.1, cov.matrix.2, nsk = 10000){
@@ -277,34 +278,39 @@ RandomSkewers <- function (cov.matrix.1, cov.matrix.2, nsk = 10000){
   return(output)
 }
 
-MantelCor <- function (corr1, corr2, nit = 1000, mod = FALSE)
-  {
-    fix <- Normlize (corr1 [lower.tri (corr1)])
-    mob <- Normlize (corr2 [lower.tri (corr2)])
-    real <- cor (cbind (fix,mob))[1,2]
-    dist <- c()
-    for (N in 1:nit)
-      {
-        shuffle <- sample (1:dim(corr1)[1])
-        mobb <- Normlize (corr2 [shuffle, shuffle] [lower.tri (corr1)])
-        dist[N] <- cor (cbind (fix,mobb))[1,2]
-      }
-    prob <- sum (dist > as.vector(real)) / nit
-    if (mod == TRUE)
-      {
-        avgM <- mean (corr1 [lower.tri(corr1)] [mob != 0])
-        avgm <- mean (corr1 [lower.tri(corr1)] [mob == 0])
-        avg.ratio <- avgM / avgm
-        output <- c(real,prob,avgM,avgm,avg.ratio)
-        names(output) <- c("R²","Probability","AVG+","AVG-","AVG Ratio")
-      }
-    else
-      {
-        output <- c(real,prob)
-        names(output) <- c("R²","Probability")
-      }
-    return (output)
+MantelCor <- function (cor.matrix.1, cor.matrix.2, nit = 1000, mod = FALSE){
+  # Calculates matrix correlation with confidence intervals using mantel permutations
+  #
+  # Args:
+  #     cor.matrix.(1,2): correlation matrices being compared
+  #     nit: number of permutations
+  #     mod: for when testing binary modularity hipotesis
+  # Results:
+  #     matrix pearson correelation and significance.
+  #     if mod==TRUE also returns average within, between and average ratio correlations
+  fixed.matrix <- cor.matrix.1 [lower.tri (cor.matrix.1)]
+  shuffled.matrix <- cor.matrix.2 [lower.tri (cor.matrix.2)]
+  correlation <- cor (fixed.matrix,shuffled.matrix)
+  shuffled.correlation <- c()
+  for (N in 1:nit){
+    shuffle <- sample (1:dim(cor.matrix.1)[1])
+    shuffled.matrix <- cor.matrix.2 [shuffle, shuffle] [lower.tri (cor.matrix.1)]
+    shuffled.correlation[N] <- cor (fixed.matrix,shuffled.matrix)
   }
+  prob <- sum (shuffled.correlation > as.vector(correlation)) / nit
+  if (mod == TRUE){
+    avg.plus <- mean (cor.matrix.1 [lower.tri(cor.matrix.1)] [mob != 0])
+    avg.minus <- mean (cor.matrix.1 [lower.tri(cor.matrix.1)] [mob == 0])
+    avg.ratio <- avg.plus / avg.minus
+    output <- c(correlation,prob,avg.plus,avg.minus,avg.ratio)
+    names(output) <- c("R²","Probability","AVG+","AVG-","AVG Ratio")
+  }
+  else{
+    output <- c(correlation,prob)
+    names(output) <- c("R²","Probability")
+  }
+  return (output)
+}
 
 papyrus <- function (vcv1, vcv2, nsk = 1000)
 {
