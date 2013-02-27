16eaaa004724d17ee6f7dbdba98874ef7d3a3c7b
Guilherme <guilherme@wgar02.(none)>
2013-02-27 17:15:42 -0300

    added function 'residulaMatrix'

--- matrix.func.R
+++ matrix.func.R
@@ -612,3 +612,16 @@ plot.group.evol <- function (evol.list)
     }
   }
 }
+
+residualMatrix <- function (model)
+  {
+    ## Calculates residual matrix from a estimated linear model
+    ## Args:
+    ##  model: linear model previously estimated
+    ## Value:
+    ##  cov.mat: residual covariance matrix
+    res <- residuals (model)
+    res.df <- model $ df.residual
+    cov.mat <- res %*% t (res) / res.df
+    return (cov.mat)
+  }
